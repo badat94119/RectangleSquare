20d31ff73a922db6fcae14cac4552e91cdb8f42b
badat94119 <datba411@gmail.com>
2023-04-02 13:24:01 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
